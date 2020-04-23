c8b922afb4d118bb80e112de2e60b68ca8b11eba
JoeKlaus <63110703+JoeKlaus@users.noreply.github.com>
2020-04-23 14:52:53 -0600

    Count to 100

--- Program.cs
+++ Program.cs
@@ -4,34 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FizzBuzz
+namespace LoopsLab
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i;
-
-            for (i = 1; i <= 100; i++)
+            int userChoice;
+            do
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine(i + " Fizz Buzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine(i + " Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine(i + " Buzz");
-                }
+                PrintMenu();
+                userChoice = int.Parse(Console.ReadLine());
                 
-                else
+
+                if (userChoice == 1)
                 {
-                    Console.WriteLine(i);
+                    CountTo100();
                 }
+                else if(userChoice == 2)
+                {
+                    CountDownFrom100();
+                }
+                else if (userChoice == 3)
+                {
+                    CountByTwos();
+                }
+            } while (userChoice != 4);
+        } // end main
+        static void CountTo100()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
             }
+        }
+        static void CountDownFrom100()
+        {
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void CountByTwos()
+        {
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("1. Count To 100");
+            Console.WriteLine("2. Count Down From 100");
+            Console.WriteLine("3. Count to 100 by 2's");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("Select one: ");
         }
     }
 }
